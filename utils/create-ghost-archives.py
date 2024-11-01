2465fbfaf90f73bf5ee3b1cd30e8dc184390fd20
Cymon J. Cox <cymon.cox@googlemail.com>
2024-11-01 22:57:57 +0000

    Added broken archive checking and workaround

--- utils/create-ghost-archives.py
+++ utils/create-ghost-archives.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-import argparse
-import logging as log
-import os
-import pathlib as Path
-import shutil
-import subprocess
+from pathlib import Path
 import sys
+import os
+import logging as log
+import argparse
 import textwrap
+import subprocess
+import shutil
 
 desc = """
 Create Ghost Archives for MetaGOflow Data Products
