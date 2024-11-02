eb4dc0b91aebf41f77436484096a1a4a3aa0d9d2
Cymon J. Cox <cymon.cox@googlemail.com>
2024-11-02 16:48:39 +0000

    Added fix archives

--- utils/create-ghost-archives.py
+++ utils/create-ghost-archives.py
@@ -18,9 +18,12 @@ consists of all the files but without any file content
 """
 
 
-def main(target_directory, debug):
-    log_level = log.DEBUG if debug else log.INFO
-    log.basicConfig(format="\t%(levelname)s: %(message)s", level=log_level)
+def main(
+    target_directory, remove_open_archives=False, do_not_fix_archive=False, debug=False
+):
+    log.basicConfig(
+        format="\t%(levelname)s: %(message)s", level=log.DEBUG if debug else log.INFO
+    )
 
     # Check the target_directory name
     target_directory = Path(target_directory)
@@ -45,43 +48,65 @@ def main(target_directory, debug):
     if len(tarball_files) == 0:
         log.error(f"Cannot find any tarball files in {target_directory}")
         sys.exit()
+    else:
+        # Where the ghost archives will go
+        Path("ghost-archives").mkdir(exist_ok=True)
+        Path("fixed-archives").mkdir(exist_ok=True)
 
     # Un bzip the tarballs
     for tarball_file in tarball_files:
-        broken = False
+        archive_without_top_level = False
 
         run_id = Path(str(tarball_file).rsplit(".", 2)[0])
         log.debug(f"run_id = {run_id}")
+        ghost_dir = Path("ghost-archives", f"{run_id}")
+        if ghost_dir.exists():
+            log.info(f"Ghost archive already exists for {run_id}")
+            continue
 
         if run_id.exists():
             log.debug("Found open archive")
         else:
-            log.info(f"Opening archive {tarball_file}...")
-            subprocess.call(["tar", "-xjf", f"{tarball_file}"])
-            # Archive without top level directory
-            if not run_id.exists():
-                broken = True
-                log.debug(f"Checking for broken archive at {run_id}")
+            # All this flaffing about with a ./temp dir is because
+            # there maybe other files in the to directory of the tarball
+            # and we want to ensure those files go into the ghost
+            # archive even if they never go to S3
+
+            # For untaring archives
+            Path("temp").mkdir(exist_ok=False)
+            os.chdir("temp")
+            log.info(f"Opening archive {tarball_file} in ./temp...")
+            subprocess.call(["tar", "-xjf", f"../{tarball_file}"])
+
+            if run_id.exists():
+                os.chdir("..")
+                # Move archive up to target directory
+                Path("temp", run_id).rename(run_id)
+                shutil.rmtree("temp")
+            else:
+                archive_without_top_level = True
+                # Archive with top level directory
+                log.debug("Checking archive in ./temp")
                 yml_files = list(Path.cwd().glob("*.yml"))
                 log.debug(f"Found {yml_files} yml files")
-                if not len(yml_files) == 2:
-                    log.error(f"Found {len(yml_files)} yml files")
-                    sys.exit()
-                if Path("results").exists():
-                    log.debug("Found broken archive")
-                    Path(run_id).mkdir()
-                    Path("results").rename(Path(run_id, "results"))
-                    for yf in yml_files:
-                        yf.rename(Path(run_id, yf.name))
+                if Path("results").exists() and len(yml_files) == 2:
+                    log.debug("Found archive without top level directory")
+                    os.chdir("..")
+                    log.debug(f"Renaming ./temp to {run_id}")
+                    Path("temp").rename(run_id)
                 else:
+                    # Deal with broken archive
                     log.error(f"Archive looks completely broken at {tarball_file}")
-                    sys.exit()
+                    os.chdir("..")
+                    shutil.rmtree("temp")
+                    log.debug("Renaming broken archive")
+                    tarball_file.rename(f"{tarball_file}-broken")
+                    continue
 
         # Recursive glob the open archive
         archive_files = run_id.rglob("*")
 
         # Make ghost archive structure
-        ghost_dir = Path(f"{run_id}" + "-ghost")
         ghost_dir.mkdir(exist_ok=False)
         Path(ghost_dir, "results").mkdir()
         Path(ghost_dir, "results", "sequence-categorisation").mkdir()
@@ -95,7 +120,13 @@ def main(target_directory, debug):
         log.info(f"Created ghost archive for {run_id}")
 
         # Remove the open archive
-        if not broken:
+        if not do_not_fix_archive and archive_without_top_level:
+            # Double neg: fix it
+            log.info(f"Fixing archive without top level directory {run_id}")
+            subprocess.call(
+                ["tar", "-cjf", f"fixed-archives/{run_id}.tar.bz2", f"{run_id}"]
+            )
+        if remove_open_archives:
             shutil.rmtree(run_id)
 
     # CD back to home directory
@@ -115,5 +146,22 @@ if __name__ == "__main__":
         help="Name of target directory containing MetaGOflow output",
     )
     parser.add_argument("-d", "--debug", action="store_true", help="DEBUG logging")
+    parser.add_argument(
+        "-r",
+        "--remove_open_archives",
+        action="store_true",
+        help="Remove the open archives after creating the ghost archives",
+    )
+    parser.add_argument(
+        "-nf",
+        "--do_not_fix_archive",
+        action="store_true",
+        help="Do not fix archives that lack a top level directory",
+    )
     args = parser.parse_args()
-    main(args.target_directory, args.debug)
+    main(
+        args.target_directory,
+        args.remove_open_archives,
+        args.do_not_fix_archive,
+        args.debug,
+    )
