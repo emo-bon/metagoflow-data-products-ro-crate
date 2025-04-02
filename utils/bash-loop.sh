5ee9a54b53dfef8c8093e98e72e1fb4c86dcdb9a
Cymon J. Cox <cymon.cox@googlemail.com>
2025-04-02 11:21:21 +0100

    Modified bash-loop.sh

--- utils/bash-loop.sh
+++ utils/bash-loop.sh
@@ -1,6 +1,23 @@
 #! /usr/bin/env bash
- 
-for f in working/ghost-archives/*; do
-    echo ./create-ro-crate.py -d $f working/config.yaml;
-    ./create-ro-crate.py -d $f working/config.yaml;
+
+# Delete objects in S3
+s5cmd --profile eosc-fairease1 --endpoint-url https://s3.mesocentre.uca.fr rm s3://mgf-data-products/*
+# Remove previous ro-crate builds
+rm -rf metaGOflow-rocrates-dvc/*ro-crate
+
+# Prepare FILTER archives
+./utils/prepare_data.py working/FILTERS
+# Build FILTER ro-crates
+for f in working/FILTERS/prepared_archives/*; do
+    echo ./create-ro-crate.py -uo $f working/config.yaml;
+    ./create-ro-crate.py -uo $f working/config.yaml;
 done
+
+# Prepare SEDIMENT archives
+./utils/prepare_data.py working/SEDIMENTS
+# Build SEDIMENT ro-crates
+for f in working/SEDIMENTS/prepared_archives/*; do
+    echo ./create-ro-crate.py -uo $f working/config.yaml;
+    ./create-ro-crate.py -uo $f working/config.yaml;
+done
+
