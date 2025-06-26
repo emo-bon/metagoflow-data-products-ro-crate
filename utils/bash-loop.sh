90da339db09f1491089fa6e7d8f1e1fc29654a43
Cymon J. Cox <cymon.cox@googlemail.com>
2025-06-26 09:55:29 +0100

    fix: trivial

--- utils/bash-loop.sh
+++ utils/bash-loop.sh
@@ -1,22 +1,24 @@
 #! /usr/bin/env bash
 
 # Delete objects in S3
-s5cmd --profile eosc-fairease1 --endpoint-url https://s3.mesocentre.uca.fr rm s3://mgf-data-products/*
+#s5cmd --profile eosc-fairease1 --endpoint-url https://s3.mesocentre.uca.fr rm s3://mgf-data-products/*
 # Remove previous ro-crate builds
-rm -rf metaGOflow-rocrates-dvc/*ro-crate
+#rm -rf metaGOflow-rocrates-dvc/*ro-crate
 
 # Prepare FILTER archives
-./utils/prepare_data.py working/FILTERS
+#./utils/prepare_data.py working/FILTERS
+# Prepare SEDIMENT archives
+#./utils/prepare_data.py working/SEDIMENTS
+
+
 # Build FILTER ro-crates
-for f in working/FILTERS/prepared_archives/*; do
+for f in working/data/FILTERS/prepared_archives/*; do
     echo ./create-ro-crate.py -uo $f working/config.yaml;
     ./create-ro-crate.py -uo $f working/config.yaml;
 done
 
-# Prepare SEDIMENT archives
-./utils/prepare_data.py working/SEDIMENTS
 # Build SEDIMENT ro-crates
-for f in working/SEDIMENTS/prepared_archives/*; do
+for f in working/data/SEDIMENTS/prepared_archives/*; do
     echo ./create-ro-crate.py -uo $f working/config.yaml;
     ./create-ro-crate.py -uo $f working/config.yaml;
 done
