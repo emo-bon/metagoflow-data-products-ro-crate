c765ae133c1b7c559857de53f49f8e4134e1429d
Cymon J. Cox <cymon@ualg.pt>
2026-02-05 15:51:53 +0000

    fix: changed default encoding for reading the shipping CSV files to 'iso-8859-1' allowing for new diacritics introduced into QC fields

--- utils/utils.py
+++ utils/utils.py
@@ -165,7 +165,8 @@ def get_refcode_and_source_mat_id_from_run_id(run_id):
 
     # for i, batch in enumerate([BATCH1_RUN_INFO_PATH, BATCH2_RUN_INFO_PATH, BATCH3_RUN_INFO_PATH]):
     for batch in [BATCH1_RUN_INFO_PATH, BATCH2_RUN_INFO_PATH]:
-        df = pd.read_csv(batch)
+        print(f"Reading {batch}")
+        df = pd.read_csv(batch, encoding='iso-8859-1')
         for row in df[["reads_name", "ref_code", "source_mat_id"]].values.tolist():
             if isinstance(row[0], str):
                 id_in_row = str(row[0].split("_")[-1])
@@ -213,7 +214,8 @@ def get_run_id_and_ref_code_from_source_mat_id(source_mat_id):
 
     # for i, batch in enumerate([BATCH1_RUN_INFO_PATH, BATCH2_RUN_INFO_PATH, BATCH3_RUN_INFO_PATH]):
     for batch in [BATCH1_RUN_INFO_PATH, BATCH2_RUN_INFO_PATH]:
-        df = pd.read_csv(batch)
+        print(f"Reading {batch}")
+        df = pd.read_csv(batch, encoding='iso-8859-1')
         for row in df[["reads_name", "ref_code", "source_mat_id"]].values.tolist():
             if isinstance(row[0], str):
                 # print(row)
